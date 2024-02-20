f8d16e9a56874f2de6bb94be74d30fe32017fb04
MustaphaMab <145995419+MustaphaMab@users.noreply.github.com>
2024-02-20 16:53:19 +0100

    ajout commande ok

--- src/Controller/CommanderController.php
+++ src/Controller/CommanderController.php
@@ -15,11 +15,36 @@ use App\Form\Commander\SocialeType;
 
 class CommanderController extends AbstractController
 {
+
+    //  ******************************************* TOUTES LES COMMANDES ******************************************************
     #[Route('/commandes', name: 'app_commandes',methods:['GET'])]
     public function index(CommanderRepository $commanderRepository): Response
     {
         return $this->render('commander/index.html.twig', [
             'commandes' =>$commanderRepository->findAllCommanderWithJointures(),
+        ]);
+    }
+
+    //  *********************************************** AJOUT COMMANDE ********************************************
+
+
+    #[Route('/commandes/ajout', name: 'ajout_commande', methods:['GET', 'POST'])]
+    public function ajout_commande(Request $request, EntityManagerInterface $entityManager,  CommanderRepository $commanderRepository ): Response
+    {
+        
+        $commande = new Commander();
+        $form = $this->createForm(CommanderType::class, $commande);
+        $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid())
+            {  
+            $entityManager->persist($commande); // persist fait le lien entre l'ORM et symfony
+            $entityManager->flush();              // flush fait le lien et applique les changements a la base de donnée
+            return $this->redirectToRoute('app_commander', [], Response::HTTP_SEE_OTHER);  // Redirigez l'utilisateur vers une autre page, par exemple la liste des livres
+            }
+
+        return $this->render('commander/ajout_commande.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
